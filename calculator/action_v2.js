296f6bc6eba8ddaba886069be9fd3f09475b3d80
CMD-Golem <64982383+CMD-Golem@users.noreply.github.com>
2023-03-25 21:47:14 +0100

    Added copy of nerdamer to repo?

---
+++ calculator/action_v2.js
@@ -0,0 +1,60 @@
+var calc_array = [];
+
+
+// ###################################################
+// Standard Functions
+function num(number) {
+	katex(number, get_pos());
+	nerdamer(number, get_pos());
+}
+
+function op(operation) {
+
+}
+
+function point() {
+
+}
+
+function special(char) {
+	
+}
+
+
+// ###################################################
+// Special Functions
+function bracket(action) {
+
+}
+
+function div(denominator) {
+
+}
+
+function pot(potency) {
+
+}
+
+function sqrt(has_degree) {
+
+}
+
+function log(log) {
+
+}
+
+function trigo(trigo) {
+
+}
+// ###################################################
+// Insert Text
+function get_pos() {
+	return 0;
+}
+
+function katex(context, pos) {
+}
+
+function nerdamer(context, pos) {
+
+}
